ed32e36a602502027dce4fce349ddfcd8c9dc399
cuicui <wyg.scut@qq.com>
2025-04-23 11:23:00 +0800

    新增了防抖

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useState, ChangeEvent } from 'react'
+import React, { FC, useState, ChangeEvent, useEffect } from 'react'
 
 import Input, { InputProps } from '../Input/input'
 import Icon from '../Icon/icon'
+import useDebounce from '../hooks/useDebounce'
 
 export interface DataSourceObject {
     value: string
@@ -17,6 +18,14 @@ export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
     renderOption?: (item: DataSourceType) => React.ReactNode
 }
 
+const formatVal = (val: string | number | readonly string[] | undefined) => {
+    if (val === undefined) {
+        return ''
+    } else {
+        return String(val)
+    }
+}
+
 const AutoComplete: FC<AutoCompleteProps> = props => {
     const {
         fetchSuggestions,
@@ -27,24 +36,38 @@ const AutoComplete: FC<AutoCompleteProps> = props => {
         ...restProps
     } = props
 
-    const [inputValue, setInputValue] = useState(value)
+    const [inputValue, setInputValue] = useState<string>(formatVal(value))
     const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
 
     const [loading, setLoading] = useState(false)
+
+    const debounceInputValue = useDebounce(inputValue, 400)
+
+    const handleFetch = async (str: string) => {
+        if (str.length !== 0) {
+            setLoading(true)
+            const res = await fetchSuggestions(str)
+            setSuggestions(res)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        console.log('inputValue', debounceInputValue)
+        handleFetch(debounceInputValue)
+    }, [debounceInputValue])
 
     const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
         onChange?.(e)
         const val = e.target.value.trim()
         setInputValue(val)
         if (val) {
-            setLoading(true)
-            const res = await fetchSuggestions(val)
-            setLoading(false)
-            setSuggestions(res)
+            // 这里要防抖，不然会频繁调用接口
         } else {
             setSuggestions([])
         }
     }
+
     const handleClickItem = (it: DataSourceType) => {
         setInputValue(it.value)
         setSuggestions([])
