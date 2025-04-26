20da1fbb68e1e6ba793320aee2c1e0b4be416c55
cuicui <wyg.scut@qq.com>
2025-04-26 17:51:11 +0800

    补充select组件的完整实现

--- example/TreeSelectExample.tsx
+++ example/TreeSelectExample.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
-import TreeSelect from '../src/components/TreeSelect/treeSelect'
-function TreeSelectExample() {
-    return <TreeSelect></TreeSelect>
+import TreeSelect, {
+    TreeNodeData,
+} from '../src/components/TreeSelect/treeSelect'
+
+const testData: TreeNodeData = {
+    label: '一级',
+    key: '1',
+    children: [
+        {
+            label: '二级',
+            key: '1-1',
+            children: [
+                {
+                    label: '三级',
+                    key: '1-1-1',
+                    children: [
+                        {
+                            state: 'all',
+                            key: '1-1-1-1',
+                            label: '四级',
+                        },
+                        {
+                            state: 'no',
+                            key: '1-1-1-2',
+                            label: '四级',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            label: '二级2',
+            key: '1-2',
+        },
+    ],
+}
+function Example() {
+    return (
+        <TreeSelect
+            onSelect={() => {
+                console.log('select')
+            }}
+            data={testData}
+        ></TreeSelect>
+    )
 }
 
-export default TreeSelectExample
+export default Example
--- src/components/TreeSelect/treeSelect.tsx
+++ src/components/TreeSelect/treeSelect.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useState } from 'react'
 import Icon from '../Icon/icon'
-export interface TreeSelectProps {
-    initData: any[]
-    onSelect: (node: any) => void
+export interface TreeNodeProps {
+    data: TreeNodeData
+    onSelect: (node: TreeNodeData) => void
 }
 
 type NodeState = 'half' | 'all' | 'no'
 
-const getIcon = (state: NodeState) => {
+const getIcon = (state?: NodeState) => {
     switch (state) {
         case 'half':
             return <Icon icon="circle-half-stroke"></Icon>
@@ -20,41 +20,138 @@ const getIcon = (state: NodeState) => {
     }
 }
 
-const data = {
-    label: '一级',
-    key: '1',
-    children: [
-        {
-            label: '二级',
-            key: '1-1',
-            children: [
-                {
-                    label: '三级',
-                    key: '1-1-1',
-                    children: [
-                        {
-                            value: 'selected',
-                            key: '1-1-1-1',
-                            label: '四级',
-                        },
-                        {
-                            value: 'no-selected',
-                            key: '1-1-1-2',
-                            label: '四级',
-                        },
-                    ],
-                },
-            ],
-        },
-    ],
+export interface TreeNodeData {
+    label: string
+    key: string
+    children?: TreeNodeData[]
+    state?: NodeState
 }
 
-const TreeNode: FC<TreeNodeProps> = ({ node, onSelect }) => {
-    const [expanded, setExpanded] = useState(false)
+// 向上更新数据
+const HeapifyUpData = (data: TreeNodeData) => {
+    if (data.children) {
+        data.children?.forEach(item => {
+            HeapifyUpData(item)
+        })
+        const childrenState = data.children?.map(item => item.state)
+        if (childrenState?.every(item => 'all' === item)) {
+            data.state = 'all'
+        } else if (childrenState?.every(item => 'no' === item)) {
+            data.state = 'no'
+        } else {
+            data.state = 'half'
+        }
+    } else {
+        // 叶子节点
+        if (!data.state) {
+            data.state = 'no'
+        }
+    }
 }
 
-const TreeSelect: FC<TreeSelectProps> = ({ data, onSelect }) => {
-    return <></>
+const getNewNodeState = (state: NodeState) => {
+    if ('all' === state) {
+        return 'no'
+    }
+    if ('no' === state || 'half' === state) {
+        return 'all'
+    }
+    return 'no'
+}
+
+const TreeNode: FC<TreeNodeProps> = ({ data, onSelect }) => {
+    return (
+        <div>
+            <div
+                onClick={() => {
+                    onSelect(data)
+                }}
+            >
+                {getIcon(data.state)}
+                {data.label}
+            </div>
+            {data.children && (
+                <div style={{ marginLeft: '20px' }}>
+                    {data.children.map((item, idx) => {
+                        return (
+                            <TreeNode
+                                key={item.key}
+                                data={item}
+                                onSelect={onSelect}
+                            ></TreeNode>
+                        )
+                    })}
+                </div>
+            )}
+        </div>
+    )
+}
+
+const dfsGetNodeByKey = (data: TreeNodeData, key: string) => {
+    if (data.key === key) {
+        return data
+    } else {
+        if (data.children) {
+            let ans = null
+            data.children.forEach(node => {
+                const res = dfsGetNodeByKey(node, key)
+                if (res) {
+                    ans = res
+                }
+            })
+            return ans
+        } else {
+            return null
+        }
+    }
+}
+
+// 将当前节点及所有子节点更新为同一个状态
+const updateNodeState = (newState: NodeState, node: TreeNodeData | null) => {
+    if (!node) {
+        return
+    }
+    node.state = newState
+    if (node.children) {
+        node.children.forEach(item => {
+            updateNodeState(newState, item)
+        })
+    }
+}
+
+const TreeSelect: FC<TreeNodeProps> = ({ data, onSelect }) => {
+    const deepCopyData = deepCopy(data)
+    HeapifyUpData(deepCopyData)
+    const [nodeData, setNodeData] = useState<TreeNodeData>(deepCopyData)
+    return (
+        <TreeNode
+            data={nodeData}
+            onSelect={node => {
+                onSelect(node)
+                const newNode = deepCopy(nodeData)
+                const targetNode = dfsGetNodeByKey(newNode, node.key)
+                const newState = getNewNodeState(targetNode?.state || 'no')
+                updateNodeState(newState, targetNode)
+                HeapifyUpData(newNode)
+                if (isDeepEqual(newNode, nodeData)) {
+                    return
+                }
+                setNodeData(newNode)
+            }}
+        ></TreeNode>
+    )
+}
+
+const deepCopy = (data: any) => {
+    return JSON.parse(JSON.stringify(data))
+}
+
+const isDeepEqual = (a: any, b: any) => {
+    if (a === b) {
+        return true
+    } else {
+        return JSON.stringify(a) === JSON.stringify(b)
+    }
 }
 
 export default TreeSelect
